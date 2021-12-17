b677623b52db8584ea8fdadc32b720d0de1b0b5c
Matt Knight <mattnite@protonmail.com>
2021-12-17 08:48:11 -0800

    works on my machine

---
+++ build.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const mbedtls = @import("mbedtls.zig");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.standardReleaseOptions();
+
+    const lib = mbedtls.create(b, target, mode);
+    lib.install();
+}
